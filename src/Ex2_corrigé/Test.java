dfd913836c0731dabca85709832c70e408eece2e
HaouariBaderdine <mimi.bader4@gmail.com>
2023-10-07 21:13:55 +0100

    07/10/2023

--- src/Ex2_corrigé/Test.java
+++ src/Ex2_corrigé/Test.java
@@ -1,44 +1,37 @@
 package Ex2_corrigé;
 
 public class Test {
-    private static final int NUM_PLAYERS = 2; // Nombre de joueurs
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        Game game = new Game();
+		Game game = new Game();
 
-        for (int i = 1; i <= NUM_PLAYERS; i++) {
-            game.addPlayer("Player " + i);
-        }
+		game.addPlayer("Player 1");
+		game.addPlayer("Player 2");
 
-        if (!game.isPlayable()) {
-            System.out.println("Le jeu n'est pas jouable avec moins de " + NUM_PLAYERS + " joueurs.");
-            return;
-        }
+		if (!game.isPlayable()) {
+			System.out.println("Le jeu n'est pas jouable avec moins de 2 joueurs.");
+			return;
+		}
 
-        GameLogic gameLogic = new GameLogic(game.getPlayers());
+		GameLogic gameLogic = new GameLogic(game.getPlayers());
 
-        System.out.println("---- Début du jeu ----");
+		while (true) {
+			int roll = (int) (Math.random() * 6) + 1;
+			System.out.println("\nNew turn!");
+			gameLogic.roll(roll);
 
-        while (true) {
-            int roll = (int) (Math.random() * 6) + 1;
-            System.out.println("\nNouveau tour !");
-            System.out.println("Le dé a montré : " + roll);
+			boolean isCorrectAnswer = Math.random() < 0.5; // 50% de chances d'une réponse correcte
+			if (isCorrectAnswer) {
+				System.out.println("Player answered correctly!");
+				gameLogic.wasCorrectlyAnswered();
+			} else {
+				System.out.println("Player answered incorrectly!");
+				gameLogic.wrongAnswer();
+			}
 
-            boolean isCorrectAnswer = Math.random() < 0.5; // 50% de chances d'une réponse correcte
-            if (isCorrectAnswer) {
-                System.out.println("Le joueur a répondu correctement !");
-                gameLogic.wasCorrectlyAnswered();
-            } else {
-                System.out.println("Le joueur a répondu incorrectement !");
-                gameLogic.wrongAnswer();
-            }
-
-            if (gameLogic.didPlayerWin()) {
-                System.out.println("\n---- Fin du jeu ----");
-                System.out.println("Le joueur gagnant est : " + gameLogic.didPlayerWin());
-                break;
-            }
-        }
-    }
+			if (gameLogic.didPlayerWin()) {
+				break;
+			}
+		}
+	}
 }
-
